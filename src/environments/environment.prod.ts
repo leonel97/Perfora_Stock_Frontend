565984e010bbd778118972fccfcdb464d9ae428f
unknown <leonel97>
2021-07-05 14:24:29 +0100

    updated dp and fp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   expirationTime: 5000000,
   backend: 'http://173.249.57.60:8081/api',
-  backend2: 'http://192.168.200.3:8080/perfora-stock/v1' 
+  backend2: 'http://192.168.200.3:8080/sigsl/perfora-stock/v1' 
 };
