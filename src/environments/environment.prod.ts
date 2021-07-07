759eea24331079a4809d636a1d6e1eaafc2816d8
unknown <leonel97>
2021-07-07 10:20:55 +0100

    updated env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   expirationTime: 5000000,
   backend: 'http://173.249.57.60:8081/api',
-  backend2: 'http://production/sigsl/perfora-stock/v1' 
+  backend2: 'http://production:8080/sigsl/perfora-stock/v1' 
 };
