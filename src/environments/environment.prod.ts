68fe54420687e827c3f9fb464c6fcbc573107a73
unknown <dossouleonel94@gmail.com>
2021-09-17 13:07:14 +0100

    updated 17/09/2021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   expirationTime: 5000000,
   backend: 'http://173.249.57.60:8081/api',
-  //backend2: 'http://192.168.200.3:8080/sigsl/perfora-stock/v1' 
-  backend2: 'http://192.168.1.158:8080/perfora-stock/v1'
+  backend2: 'http://192.168.20.5:9090/sigsl/perfora-stock/v1' 
+  //backend2: 'http://192.168.1.158:8080/perfora-stock/v1'
 };
