f27263323a79a35ed83cc333f2d3503642913654
Saliman <salimanagonhealberic@gmail.com>
2022-03-04 12:47:48 +0100

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   //backend2: 'http://192.168.20.5:9090/sigsl/perfora-stock/v1',
   //backend3: 'http://192.168.20.5:9090/sigsl'
   
-  backend2: 'http://192.168.25.5:8090/sigsl/perfora-stock/v1',
-  backend3: 'http://192.168.25.5:8090/sigsl'
+  backend2: 'http://192.168.20.5:8090/sigsl/perfora-stock/v1',
+  backend3: 'http://192.168.20.5:8090/sigsl'
   //backend2: 'http://192.168.1.158:8080/perfora-stock/v1'
 };
